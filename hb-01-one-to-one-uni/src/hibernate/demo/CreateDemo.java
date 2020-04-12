4abef57ff46cb917f3a3026271d2e3fada4e312f
rasuu1996 <58535289+rasuu1996@users.noreply.github.com>
2020-04-13 01:39:54 +0200

    One to one uni-directional mapping

---
+++ hb-01-one-to-one-uni/src/hibernate/demo/CreateDemo.java
@@ -0,0 +1,49 @@
+package hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class CreateDemo {
+
+	public static void main(String[] args) {
+// create session factory
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+
+				.buildSessionFactory();
+		// create session
+		Session session = factory.getCurrentSession();
+
+		try {
+			// create student object
+			/*Instructor tempInstructor = new Instructor("Rasika","shinde","aslkfsd@gmail.com");
+
+			InstructorDetail tempInstructorDetail = new InstructorDetail("http://www.gmail.com", "luv 2 code");
+*/
+			
+			Instructor tempInstructor = new Instructor("Gunajan","Kadu","gunjan@gmail.com");
+
+			InstructorDetail tempInstructorDetail = new InstructorDetail("http://www.gunjan.com", "luv 2 learn");
+
+			// Associate the objects
+			tempInstructor.setInstructorDetail(tempInstructorDetail);
+			// start transaction
+			session.beginTransaction();
+
+			// save the instructor
+			// note: this will also save the details object
+			// bacause of CascadeType.ALL
+			System.out.println("saving instructor:" + tempInstructor);
+			session.save(tempInstructor);
+
+			// commit transaction
+			session.getTransaction().commit();
+			System.out.println("Done!");
+
+		} finally {
+			factory.close();
+		}
+	}
+
+}
---
+++ hb-01-one-to-one-uni/src/hibernate/demo/Instructor.java
@@ -0,0 +1,98 @@
+package hibernate.demo;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "instructor")
+public class Instructor {
+
+	// annotation the class entity and map to db table
+	// define the fields
+	// annotate the class dp column
+	// create constructor
+	// create getter setter
+	// generate tiString method
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+
+	@Column(name = "email")
+	private String email;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "instructor_detail_id")
+	private InstructorDetail InstructorDetail;// InstructorDetail is object
+
+	public Instructor() {
+
+	}
+
+	public Instructor(String firstName, String lastName, String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public InstructorDetail getInstructorDetail() {
+		return InstructorDetail;
+	}
+
+	public void setInstructorDetail(InstructorDetail instructorDetail) {
+		InstructorDetail = instructorDetail;
+	}
+
+	@Override
+	public String toString() {
+		return "instructor [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", InstructorDetail=" + InstructorDetail + "]";
+	}
+
+}
